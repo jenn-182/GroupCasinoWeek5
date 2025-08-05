71affd714ea1389606473dfa21730d660f1c1b82
danishahsancs <danishahsancs@gmail.com>
2025-08-05 15:38:06 -0400

    made basis of casinoAccount class

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
@@ -6,4 +6,47 @@ package com.github.zipcodewilmington.casino;
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class CasinoAccount {
+    Double balance;
+    String username;
+    String password;
+
+    CasinoAccount(Double initBalance, String username, String password){
+        balance = initBalance;
+        this.username = username;
+        this.password = password;
+    }
+    public Double getBalance(){
+        return balance;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean checkPassword(String password){
+        if(this.password.equals(password))
+            return true;
+        return false;
+    }
+
+    public boolean resetPassword(String currentPassword, String newPassword){
+        if(checkPassword(currentPassword)){
+            password = newPassword;
+            System.out.println("Current password is correct, password has changed");
+            return true;
+        }else{
+            System.out.println("Current password is incorrect, did not change password");
+        }
+        return false;
+    }
+
+    public void deposit(double deposit){
+        balance+=deposit;
+    }
+
+    public void withdraw(double ammount){
+        balance-=ammount;
+    }
+
+
 }
