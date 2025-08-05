334e3acd6f09fe8184973df6f6d9c02b67d1ca0e
AnthonyF525 <Anthony.falice@yahoo.com>
2025-08-05 16:58:48 -0400

    Starting number wheel for roulette

--- src/main/java/com/github/zipcodewilmington/casino/games/Roulette/Roulette.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Roulette/Roulette.java
@@ -1,5 +1,17 @@
 package com.github.zipcodewilmington.casino.games.Roulette;
 
+
 public class Roulette {
+    private RouletteNumber[] wheel = new RouletteNumber[38];
+
+    public void createWheel() {
+        wheel[0] = new RouletteNumber(0, "Green");
+    }
+    
+
+    // public static void main(String[] args) {
+    //     RouletteNumber red7 = new RouletteNumber(7, "RED");
+    //     System.out.println("Number: " + red7.getNumber() + ", Color: " + red7.getColor());
+    // }
     
 }
